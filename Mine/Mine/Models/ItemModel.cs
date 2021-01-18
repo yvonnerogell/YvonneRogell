01dda4e5896ca0c19901c4e0aa1ea399120702eb
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-01-18 09:13:52 -0800

    Added comments to ItemModel.cs.

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,13 +2,21 @@
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Items for the Characters and Monsters to use.
+    /// </summary>
     public class ItemModel
     {
+        // The Id for the Item
         public string Id { get; set; }
+
+        // The Display Text for the Item
         public string Text { get; set; }
+
+        // The Description for the Item
         public string Description { get; set; }
 
-        // THe Value of the Item +9 Damage
+        // The Value of the Item +9 Damage
         public int Value { get; set; } = 0;
     }
 }
