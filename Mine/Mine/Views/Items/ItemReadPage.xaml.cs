3d17f8ef9115508653fee244de0ba780b2f29446
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-01-20 07:38:05 -0800

    Added toolbar to open DeletePage, change in ItemReadPage.Xaml, ItemReadPage.xaml.cs

--- Mine/Mine/Views/Items/ItemReadPage.xaml.cs
+++ Mine/Mine/Views/Items/ItemReadPage.xaml.cs
@@ -35,5 +35,15 @@ namespace Mine.Views
             viewModel = new ItemReadViewModel(item);
             BindingContext = viewModel;
         }
+
+        /// <summary>
+        /// Open the Delete Page for this item. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void DeleteItem_Clicked(object sender, EventArgs e)
+		{
+            await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(viewModel)));
+		}
     }
 }
