473fc0fad531477627aee0e342807f076698b752
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-01-20 08:06:00 -0800

    Added ItemUpdatePage to project, changed ItemUpdatePage.xaml, ItemUpdatePage.xaml.cs, Mine.csproj

---
+++ Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Mine.Models;
+
+namespace Mine.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemUpdatePage : ContentPage
+    {
+        public ItemModel Item { get; set; }
+
+        public ItemUpdatePage()
+        {
+            InitializeComponent();
+
+            Item = new ItemModel
+            {
+                Text = "Item name",
+                Description = "This is an item description."
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "AddItem", Item);
+            await Navigation.PopModalAsync();
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+		{
+            ValueValue.Text = String.Format("{0}", e.NewValue);
+		}
+    }
+}
