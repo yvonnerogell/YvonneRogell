85d173fff6de1d943ecfa94df93dc598e296bffb
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-01-20 08:03:32 -0800

    Updated to Pop Stack after DelteItem_Clicked.  Change in ItemReadPage.xaml

--- Mine/Mine/Views/Items/ItemReadPage.xaml.cs
+++ Mine/Mine/Views/Items/ItemReadPage.xaml.cs
@@ -44,6 +44,7 @@ namespace Mine.Views
         public async void DeleteItem_Clicked(object sender, EventArgs e)
 		{
             await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(viewModel)));
+            await Navigation.PopAsync();
 		}
     }
 }
