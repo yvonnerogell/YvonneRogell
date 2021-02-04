f9648c0bc99ffc90a267719b2804883d2fa3d8c5
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-04 10:15:09 -0800

    Added RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15 to RollRiceHelperUnitTest.cs

--- UnitTests/Helpers/RollDiceHelperUnitTest.cs
+++ UnitTests/Helpers/RollDiceHelperUnitTest.cs
@@ -114,5 +114,22 @@ namespace UnitTests.Models
             // Assert 
             Assert.AreEqual(5, result);
         }
+
+        [Test]
+        public void RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15()
+		{
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 5;
+
+            // Act
+            var result = DiceHelper.RollDice(3, 10);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert 
+            Assert.AreEqual(15, result);
+        }
     }
 }
