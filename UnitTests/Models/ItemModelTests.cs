6cc915211ebf65f17f68ef0eac97ba47a47740bb
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-04 09:28:50 -0800

    Added ItemModelTests.cs to the project.

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+
+            // Reset
+
+            // Assert 
+            Assert.Pass();
+        }
+    }
+}
