944d918521c210ad45f99fdb1a206b8a04cc09b0
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-04 09:40:59 -0800

    Added ItemModel_Set_Get to ItemModelTests.cs.

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -19,5 +19,26 @@ namespace UnitTests.Models
             // Assert 
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void ItemModel_Set_Get_Valid_Default_Should_Pass()
+		{
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+            result.Description = "Description";
+            result.Id = "ID";
+            result.Text = "Text";
+            result.Value = 1;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Description", result.Description);
+            Assert.AreEqual("ID", result.Id);
+            Assert.AreEqual("Text", result.Text);
+            Assert.AreEqual(1, result.Value);
+		}
     }
 }
