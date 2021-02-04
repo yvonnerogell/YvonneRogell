76589672eaa02c4cf38739b563d1642bbf7980d3
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-04 09:51:17 -0800

    Added DiceHelper.cs to project.

---
+++ Mine/Mine/Helpers/DiceHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mine.Helpers
+{
+    public static class DiceHelper
+    {
+        private static Random rnd = new Random();
+
+        public static bool ForceRollsToNotRandom = false;
+
+        public static int ForcedRandomValue = 1;
+
+        public static int RollDice(int rolls, int dice)
+        {
+            if (ForceRollsToNotRandom)
+            {
+                return rolls * ForcedRandomValue;
+            }
+
+            if (rolls < 1)
+            {
+                return 0;
+            }
+
+            if (dice < 1)
+            {
+                return 0;
+            }
+
+            var myReturn = 0;
+
+            for (var i = 0; i < rolls; i++)
+            {
+                myReturn += rnd.Next(1, dice + 1);
+            }
+
+            return myReturn;
+        }
+    }
+}
