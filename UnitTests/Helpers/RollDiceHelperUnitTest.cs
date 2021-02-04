8f0c5a0d67c39ca0a288323cda39ea4da2b03bef
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-04 10:01:29 -0800

    Added RollDice_Invalid_Roll_Forced_1_Should_Return_1 to RollDiceHelperUnitTest.cs.

--- UnitTests/Helpers/RollDiceHelperUnitTest.cs
+++ UnitTests/Helpers/RollDiceHelperUnitTest.cs
@@ -35,5 +35,23 @@ namespace UnitTests.Models
             Assert.AreEqual(true, result >= 1);
             Assert.AreEqual(true, result <= 6);
 		}
+
+        [Test]
+        public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
+		{
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 1;
+
+            // Act
+            var result = DiceHelper.RollDice(1, 1);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert 
+            Assert.AreEqual(1, result);
+
+        }
     }
 }
