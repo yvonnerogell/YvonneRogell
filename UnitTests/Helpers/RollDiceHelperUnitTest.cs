81e7b2f2259bc6724a578da28ab14a6c41697306
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-04 10:06:46 -0800

    Added RollDice_Invalid_Roll_1_Dice_0_Should_Return_Zero to RollDiceHelperUnitTest.cs.

--- UnitTests/Helpers/RollDiceHelperUnitTest.cs
+++ UnitTests/Helpers/RollDiceHelperUnitTest.cs
@@ -53,5 +53,19 @@ namespace UnitTests.Models
             Assert.AreEqual(1, result);
 
         }
+
+        [Test]
+        public void RollDice_Invalid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
